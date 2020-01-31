636661725701f2b51d74802be07e4138b3a4c79f
Jefferson Gomes <jefferson.g.silva@hotmail.com>
2020-01-31 14:55:33 -0300

    Configuração inicial do app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const consign = require("consign");
+
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+app.set("views", "app/views");
+
+app.use(express.static("./app/public"));
+
+consign()
+  .include("app/routes")
+  .then("app/controllers")
+  .then("app/models")
+  .into(app);
+
+app.listen(port);
